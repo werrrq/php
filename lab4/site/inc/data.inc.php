d98bb18a18f22c18d6c1d9a446fe471b4cee0d1c
werrrq <andrekozin@icloud.com>
2025-12-08 23:05:58 +0300

    Create data.inc.php

---
+++ lab4/site/inc/data.inc.php
@@ -0,0 +1,11 @@
+
+<?php
+declare(strict_types=1);
+
+$leftMenu = [
+    ['link' => 'Домой', 'href' => 'index.php'],
+    ['link' => 'О нас', 'href' => 'index.php?id=about'],
+    ['link' => 'Контакты', 'href' => 'index.php?id=contact'],
+    ['link' => 'Таблица умножения', 'href' => 'index.php?id=table'],
+    ['link' => 'Калькулятор', 'href' => 'index.php?id=calc'],
+];
