e9b60a6a367620a0662dd83f9ba4f892afbf1cee
werrrq <andrekozin@icloud.com>
2025-12-08 23:05:43 +0300

    Create bottom.inc.php

---
+++ lab4/site/inc/bottom.inc.php
@@ -0,0 +1,10 @@
+
+<footer>
+    <!-- Нижняя часть страницы -->
+    <?php 
+        $dateArray = getdate();
+        $year = $dateArray['year'];
+    ?>
+    &copy; Супер Мега Веб-мастер, 2000 &ndash; <?= $year ?>
+    <!-- Нижняя часть страницы -->
+</footer>
