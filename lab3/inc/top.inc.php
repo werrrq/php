9e1fea8bf40ad30e84094e9441e382018b8a6322
werrrq <andrekozin@icloud.com>
2025-12-08 22:56:14 +0300

    Create top.inc.php

---
+++ lab3/inc/top.inc.php
@@ -0,0 +1,7 @@
+
+<header>
+    <!-- Верхняя часть страницы -->
+    <img src="logo.png" width="130" height="80" alt="Наш логотип" class="logo">
+    <span class="slogan">приходите к нам учиться</span>
+    <!-- Верхняя часть страницы -->
+</header>
