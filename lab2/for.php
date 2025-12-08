511a6ce6167037ff4e793aae8bbe9a16d2127410
werrrq <andrekozin@icloud.com>
2025-12-08 22:51:37 +0300

    Update for.php

--- lab2/for.php
+++ lab2/for.php
@@ -1,1 +1,25 @@
-
+<?php declare(strict_types=1); ?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Цикл for</title>
+</head>
+<body>
+	<h1>Цикл for</h1>
+	<?php
+	/*
+	ЗАДАНИЕ
+	- Используя цикл for выведите в столбик Нечётные числа от 1 до 50
+	*/
+    
+    // Цикл от 1 до 50
+    // $i += 2 — это оптимизация, мы сразу шагаем через один (1, 3, 5...)
+    for ($i = 1; $i <= 50; $i += 2) {
+        echo $i . "<br>"; // <br> делает перенос строки (столбик)
+    }
+	?> 
+</body>
+</html>
