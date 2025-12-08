d120a0fd65ed74c12eaf825715a4739ba6e220dd
werrrq <andrekozin@icloud.com>
2025-12-08 22:53:42 +0300

    Update if.php

--- lab1/if.php
+++ lab1/if.php
@@ -1,1 +1,29 @@
+<?php
+// ЗАДАНИЕ 1
+$age = 20; // произвольное числовое значение
+?>
 
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Конструкции if-elseif-else</title>
+</head>
+<body>
+<h1>Конструкции if-elseif-else</h1>
+<?php
+// ЗАДАНИЕ 2
+if ($age >= 18 && $age <= 59) {
+    echo 'Вам ещё работать и работать';
+} elseif ($age > 59) {
+    echo 'Вам пора на пенсию';
+} elseif ($age >= 1 && $age <= 17) {
+    echo 'Вам ещё рано работать';
+} else {
+    echo 'Неизвестный возраст';
+}
+?>
+</body>
+</html>
