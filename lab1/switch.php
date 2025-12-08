dcbe1b176117b30e9145a6a43452bfb12e7173e9
werrrq <andrekozin@icloud.com>
2025-12-08 22:53:54 +0300

    Update switch.php

--- lab1/switch.php
+++ lab1/switch.php
@@ -1,1 +1,35 @@
+<?php
+// ЗАДАНИЕ 1
+$day = 4; // произвольное целочисленное значение
+?>
 
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Конструкция switch</title>
+</head>
+<body>
+<h1>Конструкция switch</h1>
+<?php
+// ЗАДАНИЕ 2
+switch ($day) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        echo 'Это рабочий день';
+        break;
+    case 6:
+    case 7:
+        echo 'Это выходной день';
+        break;
+    default:
+        echo 'Неизвестный день';
+}
+?>
+</body>
+</html>
