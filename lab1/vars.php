548fa60ad60ac6c34aeb98feb8658da5b7921fd8
werrrq <andrekozin@icloud.com>
2025-12-08 22:54:06 +0300

    Update vars.php

--- lab1/vars.php
+++ lab1/vars.php
@@ -1,1 +1,29 @@
+<?php
+// ЗАДАНИЕ 1
+$name = 'Андрей';
+$age = 20;
 
+// ЗАДАНИЕ 2
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Переменные и вывод</title>
+</head>
+<body>
+<h1>Переменные и вывод</h1>
+<?php
+echo "Меня зовут: $name<br>";
+echo "Мне $age лет<br>";
+echo "Тип переменной \$name: " . gettype($name) . "<br>";
+echo "Тип переменной \$age: " . gettype($age) . "<br>";
+
+// Удаление переменных
+unset($name, $age);
+?>
+</body>
+</html>
