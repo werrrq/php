f7eddcebfc0842edf6e66b758aedf2aae85f2271
werrrq <andrekozin@icloud.com>
2025-12-19 18:04:30 +0300

    Delete lab6/menu.inc.php

--- lab6/menu.inc.php
+++
@@ -1,7 +0,0 @@
-<?php declare(strict_types=1); ?>
-<h2>Меню</h2>
-<ul>
-	<li><a href="page1.php">Страница 1</a></li>
-	<li><a href="page2.php">Страница 2</a></li>
-	<li><a href="page3.php">Страница 3</a></li>
-</ul>
