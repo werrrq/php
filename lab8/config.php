1e1b93c1c264a9edba7bb0c29a2a58d3e30b4ffd
werrrq <andrekozin@icloud.com>
2025-12-19 17:57:49 +0300

    Update and rename config-sample.php to config.php

--- lab8/config.php
+++ lab8/config.php
@@ -1,7 +1,7 @@
 <?php
 /* Пример настройки подключения (образец) */
 define('DB_HOST', 'localhost');
-define('DB_USER', 'your_username');
-define('DB_PASSWORD', 'your_password');
-define('DB_NAME', 'your_db_name');
+define('DB_USER', 'f1195342_login_db');
+define('DB_PASSWORD', 'Lol123456!');
+define('DB_NAME', 'f1195342_login_db');
 define('DB_CHARSET', 'utf8mb4');
