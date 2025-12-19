9058f5ff81bc53021fe53d7dc8c5409153b20bb4
werrrq <andrekozin@icloud.com>
2025-12-19 14:50:07 +0300

    Create config-sample.php

---
+++ lab8/config-sample.php
@@ -0,0 +1,7 @@
+<?php
+/* Пример настройки подключения (образец) */
+define('DB_HOST', 'localhost');
+define('DB_USER', 'your_username');
+define('DB_PASSWORD', 'your_password');
+define('DB_NAME', 'your_db_name');
+define('DB_CHARSET', 'utf8mb4');
